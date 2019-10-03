57943c3e6ae47ba849ee692b6b8daa45ca158070
RashSR <reinhold.schlager@gmx.de>
2019-10-03 16:10:53 +0200

    Added Heartbeat to RED and YELLOW.

--- ResidentEvil3/src/ResInv/HealthStatus.java
+++ ResidentEvil3/src/ResInv/HealthStatus.java
@@ -5,17 +5,26 @@ import javax.swing.JLabel;
 
 public class HealthStatus extends Thread{
 	public static int healthState = 0; //Zeigt an in welchem Healtstatus der Spieler gerade ist
-	public static boolean running=false; //Gibt an ob Thread läuft
-	public static JLabel[] healthBeatFULL=new JLabel[26]; //einzelne JLabels für die healtbars
-	
+	public static JLabel[] healthBeatFULL=new JLabel[26]; //einzelne JLabels für die full healthbars
+	public static JLabel[] healthBeatHARMED=new JLabel[32]; //einzelne JLabels für die harmed healthbars
+	public static JLabel[] healthBeatWOUNDED=new JLabel[39]; //einzelne JLabels für die wounded healthbars
+	private int heartSpeed=100; //Gibt die Geschwindigkeit der Herzschläge an
+	//Konstruktor lädt die Dateien ins JLabel-Array
 	public HealthStatus() {
-		running=true;
-		
-		for(int i=0; i<26;i++) {
-			healthBeatFULL[i]=new JLabel(new ImageIcon("rsc/fullH/fullhealth"+i+".png"));
-			healthBeatFULL[i].setBounds(103, 42, 150, 100);
+		for(int i=0; i<healthBeatWOUNDED.length;i++) {
+			if(i<healthBeatFULL.length) {
+				healthBeatFULL[i]=new JLabel(new ImageIcon("rsc/fullH/fullhealth"+i+".png"));
+				healthBeatFULL[i].setBounds(103, 42, 150, 100);
+			}
+			if(i<healthBeatHARMED.length) {
+				healthBeatHARMED[i]=new JLabel(new ImageIcon("rsc/harM/harmed"+i+".png"));
+				healthBeatHARMED[i].setBounds(103, 42, 150, 100);
+			}
+			if(i<healthBeatWOUNDED.length) {
+				healthBeatWOUNDED[i]=new JLabel(new ImageIcon("rsc/wounD/wounded"+i+".png"));
+				healthBeatWOUNDED[i].setBounds(103, 42, 150, 100);
+			}
 		}
-		
 	}
 	//Setzt das Leben des Spielers zurück
 	public static void decreaseHealthStatus() {
@@ -24,7 +33,6 @@ public class HealthStatus extends Thread{
 			healthState--;
 			return;
 		}
-		showHealthState();
 	}
 	//Erhöht das Leben des Spielers falls er ein heilendes Kraut isst
 	public static void eatHerb(HerbType h) {
@@ -41,7 +49,6 @@ public class HealthStatus extends Thread{
 			return;
 		}
 		removeHerb();
-		showHealthState();
 		GUI.fillItemDescriptionArray(true);
 	}
 	//Entfernt das Kraut aus dem Inventar und GUI
@@ -54,40 +61,73 @@ public class HealthStatus extends Thread{
 	public static void showHealthState() {
 		GUI.fillStatusArrays(true);
 	}
+	//Thread zeichnet den Herzschlag ins GUI. Bilder werden nacheinander angezeigt.
 	@Override
 	public void run() {
 		int state = healthState;
 		int zaehler=0;
-		while(running) {
+		int speed=1;
+		while(!Thread.interrupted()) {
+			GUI.statusTexture[state].setVisible(false);
 			if(state==HealthState.FULLHEALTH.getHealthState()) {
+				speed=heartSpeed;
 				if(zaehler==0) {
-					GUI.statusTexture[state].setVisible(false);
 					GUI.statusTexture[HealthState.FULLHEALTH.getHealthState()] = healthBeatFULL[0];
 					GUI.nemesisLabel.add(GUI.statusTexture[HealthState.FULLHEALTH.getHealthState()]);
-					//System.out.println("ist jetzt wieder voll");
 				}else {
-					GUI.statusTexture[state].setVisible(false);
 					GUI.statusTexture[HealthState.FULLHEALTH.getHealthState()] = healthBeatFULL[zaehler];
 					GUI.nemesisLabel.add(GUI.statusTexture[HealthState.FULLHEALTH.getHealthState()]);
-					//System.out.println("sollte was anderes machen");
 				}
 				zaehler++;
-				if(zaehler==26) {
+				if(zaehler==healthBeatFULL.length) {
 					zaehler=0;
 				}
-				showHealthState();
+			}else if(state==HealthState.HARMED.getHealthState()) {
+				speed=heartSpeed*3/4;
+				if(zaehler==0) {
+					GUI.statusTexture[HealthState.HARMED.getHealthState()] = healthBeatHARMED[0];
+					GUI.nemesisLabel.add(GUI.statusTexture[HealthState.HARMED.getHealthState()]);
+				}else {
+					GUI.statusTexture[HealthState.HARMED.getHealthState()] = healthBeatHARMED[zaehler];
+					GUI.nemesisLabel.add(GUI.statusTexture[HealthState.HARMED.getHealthState()]);
+				}
+				zaehler++;
+				if(zaehler==healthBeatHARMED.length) {
+					zaehler=0;
+				}
+			}else if(state==HealthState.WOUNDED.getHealthState()) {
+				speed=heartSpeed*1/2;
+				if(zaehler==0) {
+					GUI.statusTexture[HealthState.WOUNDED.getHealthState()] = healthBeatWOUNDED[0];
+					GUI.nemesisLabel.add(GUI.statusTexture[HealthState.WOUNDED.getHealthState()]);
+				}else {
+					GUI.statusTexture[HealthState.WOUNDED.getHealthState()] = healthBeatWOUNDED[zaehler];
+					GUI.nemesisLabel.add(GUI.statusTexture[HealthState.WOUNDED.getHealthState()]);
+				}
+				zaehler++;
+				if(zaehler==healthBeatWOUNDED.length) {
+					zaehler=0;
+				}
 			}
+			showHealthState();
 			if(state!=healthState) {
-				System.out.println("Es gab eine Veränderung.");
+				if(((state==HealthState.WOUNDED.getHealthState() || state==HealthState.HARMED.getHealthState())&&HealthState.FULLHEALTH.getHealthState()==healthState)||healthState==HealthState.WOUNDED.getHealthState()&&HealthState.HARMED.getHealthState()==state) {
+					GUI.statusTexture[HealthState.HARMED.getHealthState()] = healthBeatHARMED[0];
+					GUI.statusTexture[HealthState.HARMED.getHealthState()].setVisible(false);
+				}else if(healthState==HealthState.HARMED.getHealthState()&&HealthState.FULLHEALTH.getHealthState()==state) {
+					GUI.statusTexture[HealthState.FULLHEALTH.getHealthState()] = healthBeatFULL[0];
+					GUI.statusTexture[HealthState.FULLHEALTH.getHealthState()].setVisible(false);
+				}else if((healthState==HealthState.HARMED.getHealthState() || healthState==HealthState.FULLHEALTH.getHealthState())&&HealthState.WOUNDED.getHealthState()==state) {
+					GUI.statusTexture[HealthState.WOUNDED.getHealthState()] = healthBeatWOUNDED[0];
+					GUI.statusTexture[HealthState.WOUNDED.getHealthState()].setVisible(false);
+				}
 				state=healthState;
-			}else {
-				//System.out.println("der state ist ja der selbe");
+				zaehler=0;
 			}
-			//System.out.println("Mein Status = "+healthState+" der erste status: "+state);
 			try {
-				Thread.sleep(90);
+				Thread.sleep(speed);
 			} catch (InterruptedException e) {
-				System.out.println("Es gab ein Problem!");
+				
 			}
 		}
 	}
